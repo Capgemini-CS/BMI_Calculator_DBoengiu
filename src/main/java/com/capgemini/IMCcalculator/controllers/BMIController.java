305bb21c8711ac7778920840ae85905c54c77fe9
Boengiu <darius.boengiu@capgemini.com>
2022-03-29 11:13:04 +0300

    Added spring validators

--- src/main/java/com/capgemini/IMCcalculator/controllers/BMIController.java
+++ src/main/java/com/capgemini/IMCcalculator/controllers/BMIController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.constraints.Pattern;
+
 @RestController
 public class BMIController {
 
@@ -20,8 +22,15 @@ public class BMIController {
     }
 
     @GetMapping("/BMI")
-    public ResponseEntity<BMIDto> getBMI(@RequestParam String height,
-                                         @RequestParam String mass) {
+    public ResponseEntity<BMIDto> getBMI(@RequestParam @Pattern(
+                                                                regexp = "^[1-9]",
+                                                                message = "Height should be greater than zero")
+                                         String height,
+
+                                         @RequestParam @Pattern(
+                                                 regexp = "^[1-9]",
+                                                 message = "Mass should be greater than zero")
+                                         String mass) {
         return bmiService.calculateBMI(height, mass);
     }
 }
